bc0489b8a02dd2c840c27388579011d5cddb8763
Ragini Sonar <raginisonar@gmail.com>
2020-06-02 17:49:18 +0100

    Added Select Dropdown.

--- Ajax/main.js
+++ Ajax/main.js
@@ -11,4 +11,19 @@ $(document).ready(function(){
             });
         }
     });
+
+    $('#select').change(function(){
+        var genre = $('#select').val();
+        $.ajax({
+            method: 'GET',
+            url: 'https://yts.mx/api/v2/list_movies.json?genre='+genre ,
+            success: function(data){ 
+                $movies.empty();
+                $.each(data.data.movies, function(i, movie){
+                    $movies.append('<img src="'+movie.medium_cover_image+'"><p>'+movie.title+'</p><br>');
+                });
+            },
+        });
+    });
+
 });
