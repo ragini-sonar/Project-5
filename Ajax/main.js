d2007373e77ff1549d98320581bf10f2376f4409
alineresendecampos <alineresendecampos@users.noreply.github.com>
2020-06-02 18:23:08 +0100

    Added search bar

--- Ajax/main.js
+++ Ajax/main.js
@@ -1,29 +1,59 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  var $movies = $("#movies");
 
-    var $movies = $('#movies');
+  $.ajax({
+    method: "GET",
+    url: "https://yts.mx/api/v2/list_movies.json",
+    success: function (data) {
+      $.each(data.data.movies, function (i, movie) {
+        $movies.append(
+          '<tr><td><img src="' +
+            movie.medium_cover_image +
+            '"></td><td>' +
+            movie.title +
+            "</td></tr>"
+        );
+      });
+    },
+  });
 
+  $("#select").change(function () {
+    var genre = $("#select").val();
     $.ajax({
-        method: 'GET',
-        url: 'https://yts.mx/api/v2/list_movies.json',
-        success: function(data){
-            $.each(data.data.movies, function(i, movie){
-                $movies.append('<img src="'+movie.medium_cover_image+'"><p>'+movie.title+'</p><br>');
-            });
-        }
-    });
-
-    $('#select').change(function(){
-        var genre = $('#select').val();
-        $.ajax({
-            method: 'GET',
-            url: 'https://yts.mx/api/v2/list_movies.json?genre='+genre ,
-            success: function(data){ 
-                $movies.empty();
-                $.each(data.data.movies, function(i, movie){
-                    $movies.append('<img src="'+movie.medium_cover_image+'"><p>'+movie.title+'</p><br>');
-                });
-            },
+      method: "GET",
+      url: "https://yts.mx/api/v2/list_movies.json?genre=" + genre,
+      success: function (data) {
+        $movies.empty();
+        $.each(data.data.movies, function (i, movie) {
+          $movies.append(
+            '<tr><td><img src="' +
+              movie.medium_cover_image +
+              '"></td><td>' +
+              movie.title +
+              "</td></tr>"
+          );
         });
+      },
     });
+  });
 
+  $("#searchMovie").on("keyup", function (e) {
+    let title = e.target.value;
+    $.ajax({
+      method: "GET",
+      url: "https://yts.mx/api/v2/list_movies.json?query_term=" + title,
+      success: function (data) {
+        $movies.empty();
+        $.each(data.data.movies, function (i, movie) {
+          $movies.append(
+            '<tr><td><img src="' +
+              movie.medium_cover_image +
+              '"></td><td>' +
+              movie.title +
+              "</td></tr>"
+          );
+        });
+      },
+    });
+  });
 });
